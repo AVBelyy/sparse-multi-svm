da7e0f2e00ea774d87c60978acdec020abf3cd44
Anton Belyy <anton.belyy@gmail.com>
2018-07-06 01:51:03 +0200

    Update MPegasos and add LassoPegasos

--- experiments/pegasos.py
+++ experiments/pegasos.py
@@ -14,7 +14,7 @@ from sklearn.metrics import f1_score
 from sklearn.preprocessing import normalize
 
 from lib.sparse_tools import dense_sparse_dot, dense_sparse_add, sparse_sparse_dot
-from lib.argmax_tools import ANNArgmax, BruteforceArgmax
+from lib.argmax_tools import ANNArgmax, BruteforceArgmax, RandomArgmax
 from tqdm import tqdm
 
 # Read the dataset.
@@ -23,9 +23,12 @@ out_dir = "../data/parsed"
 
 # dataset_name = "WIKI_100K"
 dataset_name = "LSHTC1"
+is_lasso = False
+# dataset_name = "20newsgroups"
 if len(sys.argv) > 1:
     dataset_name = sys.argv[1]
-# dataset_name = "20newsgroups"
+if len(sys.argv) > 2:
+    is_lasso = (sys.argv[2] == "lasso")
 
 with open(os.path.join(out_dir, "%s_train.dump" % dataset_name), "rb") as fin:
     X_train = pickle.load(fin)
@@ -46,7 +49,11 @@ for dataset_part in ("train", "heldout", "test"):
         labels = pickle.load(fin)
         n_classes = max(n_classes, max(labels) + 1)
 
-# X_train, X_test = normalize(X_train, norm="l1"), normalize(X_test, norm="l1")
+tfidf = TfidfTransformer()
+tfidf.fit(X_train)
+X_train = tfidf.transform(X_train, copy=False)
+X_heldout = tfidf.transform(X_heldout, copy=False)
+X_test = tfidf.transform(X_test, copy=False)
 
 X_train = ss.hstack([X_train, np.ones(X_train.shape[0]).reshape(-1, 1)])
 X_heldout = ss.hstack([X_heldout, np.ones(X_heldout.shape[0]).reshape(-1, 1)])
@@ -84,6 +91,7 @@ def predict(X, W_nz, nz_to_z):
             pred.append(class_id)
     return pred
 
+
 def predict_dot(X, index):
     pred = []
     for x_chunk in chunks(X, predict_chunk_size):
@@ -92,6 +100,7 @@ def predict_dot(X, index):
             class_id = nn_ids[0]
             pred.append(class_id)
     return pred
+
 
 # Load Iris datasets
 # from sklearn.datasets import load_iris
@@ -158,7 +167,9 @@ class WeightMatrix:
         self.dtype = dtype
         self.a = 1.0
         self.snorm = 0.
-        self.m = np.array([ss.csr_matrix((1, d), dtype=dtype) for _ in range(n)])
+        # self.m = np.array([ss.csr_matrix((1, d), dtype=dtype) for _ in range(n)])
+        # TODO: random_state projeban
+        self.m = np.array([(ss.random(1, d, format="csr", density=0.001, dtype=dtype) * 1. / d) for _ in range(n)])
 
     def sparse_dot(self, ix: int, v: ss.csr_matrix):
         return sparse_sparse_dot(self.m[ix], v) * self.a
@@ -169,6 +180,19 @@ class WeightMatrix:
         new_ix_norm = np.dot(self.m[ix].data, self.m[ix].data)
         self.snorm += (new_ix_norm - old_ix_norm) * (self.a * self.a)
         return self.m[ix] * self.a
+
+    def soft_threshold(self, ix: int, th: float):
+        th /= self.a
+        arr = self.m[ix].toarray()
+        gt_ix = (arr > +th)
+        lt_ix = (arr < -th)
+        eq_ix = (~gt_ix & ~lt_ix)
+        arr[gt_ix] -= th
+        arr[lt_ix] += th
+        arr[eq_ix] = 0
+        arr_sparse = ss.csr_matrix(arr, dtype=self.m[ix].dtype)
+        self.m[ix] = arr_sparse
+        return arr_sparse * self.a
 
     def scale(self, s: float):
         if abs(s) < 1e-32:
@@ -228,19 +252,26 @@ def stochastic_pegasos(X: np.array, y: np.array, pos_class: int, random_seed=Non
     return avg_wv.a * avg_wv.v
 
 
-def multi_pegasos(X: np.array, y: np.array, random_seed=None) -> WeightMatrix:
+def multi_pegasos(X: np.array, y: np.array, lasso_svm=True, random_seed=None) -> WeightMatrix:
     n, d = X.shape
 
     # TODO: make parameters
-    max_iter = 300
-    lambd = 10.
-    k = 100
+    max_iter = 3000
+    eta0 = 0.1
+    eta_decay_rate = 0.01
+
+    if lasso_svm:
+        k = int(np.sqrt(n_classes))
+        lambd = 1.
+    else:
+        k = int(np.sqrt(n_classes))
+        lambd = 1.
 
     W = WeightMatrix((n_classes, d))
     # Wyx = WeightVector(n)
 
-    # amax = BruteforceArgmax(W)
-    amax = ANNArgmax()
+    # amax1 = BruteforceArgmax(W)
+    amax2 = ANNArgmax(n_classes)
 
     if random_seed is not None:
         np.random.seed(random_seed)
@@ -259,14 +290,28 @@ def multi_pegasos(X: np.array, y: np.array, random_seed=None) -> WeightMatrix:
     with open("log_%s.txt" % dataset_name, "w") as fout:
         fout.write("i,learning_time,maf1,mif1,maf1_dot,mif1_dot,amax_multiplier,nnz_sum,sparsity\n")
 
+    # a, b = 0., 0.
     for i in tqdm(range(max_iter)):
         iter_start = time.time()
         x_ids = random_ids[i * k: (i + 1) * k]
         xs = X[x_ids]
-        eta = 1. / (lambd * (1. * np.ceil((i + 1) / 1.) + 1))
+        eta = eta0 / (1 + eta_decay_rate * i)
 
         ys = y[x_ids]
-        rs = amax.query(xs, ys)
+        # rs1 = amax1.query(xs, ys)
+        rs2 = amax2.query(xs, ys)
+        # keks1 = np.array([W.sparse_dot(r_, x_) for r_, x_ in zip(rs1, xs)])
+        # keks2 = np.array([W.sparse_dot(r_, x_) for r_, x_ in zip(rs2, xs)])
+        # kek = (keks1 - keks2)
+        # assert np.all(kek >= -1e-9)
+        # print(ys, rs1, rs2)
+        # if np.any(kek >= 1e-9):
+        #     # TODO: хотелось бы понять, почему query иногда не "видит" всех векторов в индексе
+        #     print("wombat")
+        # a += np.sum(kek <= 1e-9)
+        # b += xs.shape[0]
+        # print("Accuracy score: %.6f" % (a / b))
+        rs = rs2
         grad_ixs, grad_weights = [], []
 
         for j_, y_, r_, x_ in zip(x_ids, ys, rs, xs):
@@ -280,11 +325,13 @@ def multi_pegasos(X: np.array, y: np.array, random_seed=None) -> WeightMatrix:
                 grad_weights.append(+eta / k)
                 grad_ixs.append((r_, j_))
                 grad_weights.append(-eta / k)
+
         # Scale weight matrix and Wyx cache matrix
-        iter_scale = 1. - eta * lambd
-        W.scale(iter_scale)
-        amax_multiplier *= iter_scale
-        # Wyx.scale(iter_scale)
+        if not lasso_svm:
+            iter_scale = 1. - eta * lambd
+            W.scale(iter_scale)
+            amax_multiplier *= iter_scale
+            # Wyx.scale(iter_scale)
         # Add sub-gradients and project rows onto a sphere of r=1
         amax_update = {}
         for (class_ix, obj_ix), grad_w in zip(grad_ixs, grad_weights):
@@ -293,26 +340,32 @@ def multi_pegasos(X: np.array, y: np.array, random_seed=None) -> WeightMatrix:
             # Incrementally update Wyx (<w_yk, xk>) cache matrix
             # for x_ix in classes_objects[class_ix]:
             #     Wyx.elem_add(x_ix, sparse_sparse_dot(X.getrow(x_ix), obj) * grad_w)
-            if upd.nnz > 0:
-                upd.data /= amax_multiplier
+            upd.data /= amax_multiplier
+            amax_update[class_ix] = upd
+        # Do soft thresholding for lasso SVM
+        if lasso_svm:
+            # TODO: change gamma parameter dynamically
+            W_ixs = list(set(ys) | set(rs))
+            th = 0.00001 * n_classes / len(W_ixs) * lambd * eta
+            for class_ix in W_ixs:
+                upd = W.soft_threshold(class_ix, th)
                 amax_update[class_ix] = upd
-                # TODO: we should delete zero vectors
+
         # Normalize weight matrix and Wyx cache matrix
-        iter_norm = min(1., 1. / np.sqrt(lambd * W.snorm))
-        W.scale(iter_norm)
-        amax_multiplier *= iter_norm
-        # Wyx.scale(iter_norm)
+        if not lasso_svm:
+            iter_norm = min(1., 1. / np.sqrt(lambd * W.snorm))
+            W.scale(iter_norm)
+            amax_multiplier *= iter_norm
+            # Wyx.scale(iter_norm)
         if len(amax_update) > 0:
-            class_ixs = list(amax_update.keys())
+            class_ixs = np.array(list(amax_update.keys()))
             new_values = ss.vstack(list(amax_update.values()))
-            amax.update(class_ixs, new_values)
+            # print(class_ixs)
+            # amax1.update(class_ixs, new_values)
+            amax2.update(class_ixs, new_values)
 
         iter_end = time.time()
         learning_time += iter_end - iter_start
-
-        # if i % 100 == 0 and W.m[12].nnz > 0:
-        #     res = amax.index.knnQueryBatch(W.m[12] * W.a, k=1)
-        #     print(res)
 
         if i % 100 == 0 and i > 0:
             # Save intermediate W matrix
@@ -327,7 +380,7 @@ def multi_pegasos(X: np.array, y: np.array, random_seed=None) -> WeightMatrix:
             nnz_sum = sum([x.nnz for x in W.m])
             sparsity = nnz_sum / (len(W.m) * W.m[0].shape[1])
             y_pred_heldout = predict(X_heldout, W_nz, nz_to_z)
-            y_pred_heldout_dot = predict_dot(X_heldout, amax.index)
+            y_pred_heldout_dot = predict_dot(X_heldout, amax2.index)
             maf1 = f1_score(y_heldout, y_pred_heldout, average="macro")
             mif1 = f1_score(y_heldout, y_pred_heldout, average="micro")
             maf1_dot = f1_score(y_heldout, y_pred_heldout_dot, average="macro")
@@ -343,8 +396,8 @@ def multi_pegasos(X: np.array, y: np.array, random_seed=None) -> WeightMatrix:
 
 if __name__ == "__main__":
     # Train
-    print("processing %s ..." % dataset_name)
-    W = multi_pegasos(X_train, y_train, random_seed=0)
+    print("processing %s ... (lasso = %d)" % (dataset_name, is_lasso))
+    W = multi_pegasos(X_train, y_train, lasso_svm=is_lasso, random_seed=0)
     with open("W_%s.dump" % dataset_name, "wb") as fout:
         pickle.dump(W, fout)
     # clf = LogisticRegression(C=100.0, fit_intercept=False)
--- lib/argmax_tools.py
+++ lib/argmax_tools.py
@@ -1,5 +1,8 @@
+import random
+
 import nmslib
 import numpy as np
+import scipy.sparse as ss
 from abc import ABCMeta
 from abc import abstractmethod
 
@@ -26,12 +29,17 @@ class BruteforceArgmax(BaseArgmax):
     def query(self, xs, ys):
         indices = []
         for x, y in zip(xs, ys):
-            max_dist, max_ix = self.min_float, -1
+            max_dist, max_ixs = self.min_float, []
             for i, w in enumerate(self.W.m):
                 dist = sparse_sparse_dot(w, x)
-                if max_dist < dist and i != y:
-                    max_dist = dist
-                    max_ix = i
+                if i != y:
+                    if max_dist < dist:
+                        max_dist = dist
+                        max_ixs = [i]
+                    elif max_dist == dist:
+                        max_ixs.append(i)
+            assert len(max_ixs) > 0
+            max_ix = random.sample(max_ixs, 1)[0]
             indices.append(max_ix)
         return indices
 
@@ -39,8 +47,24 @@ class BruteforceArgmax(BaseArgmax):
         pass
 
 
+class RandomArgmax(BaseArgmax):
+    def __init__(self, n_classes):
+        self.classes = set(range(n_classes))
+
+    def query(self, xs, ys):
+        indices = []
+        for x, y in zip(xs, ys):
+            choice_set = self.classes - {y}
+            ix = random.sample(choice_set, 1)[0]
+            indices.append(ix)
+        return indices
+
+    def update(self, ixs, new_values):
+        pass
+
+
 class ANNArgmax(BaseArgmax):
-    def __init__(self, method="sw-graph", is_sparse=True):
+    def __init__(self, n_classes, method="sw-graph", is_sparse=True):
         if is_sparse:
             self.index = nmslib.init(method=method, space="negdotprod_sparse_fast",
                                      data_type=nmslib.DataType.SPARSE_VECTOR)
@@ -48,30 +72,51 @@ class ANNArgmax(BaseArgmax):
             self.index = nmslib.init(method=method, space="negdotprod",
                                      data_type=nmslib.DataType.DENSE_VECTOR)
         self.present = set()
-        # TODO: replace inadequately high values to lower ones
-        self.index.createIndex({'indexThreadQty': 4})
+        self.not_present = set(range(n_classes))
+        self.index.createIndex({"indexThreadQty": 4, "efConstruction": 2000, "NN": 100})
+        self.index.setQueryTimeParams({"efSearch": 2000})
+
+    def take_random_zero_vector(self):
+        if len(self.not_present) > 0:
+            return random.sample(self.not_present, 1)[0]
 
     def query(self, xs, ys, num_threads=4):
         results = self.index.knnQueryBatch(xs, k=2, num_threads=num_threads)
         indices = []
         # dists = []
-        for (ixs, ds), y in zip(results, ys):
+        for (ixs, ds), y_ in zip(results, ys):
             if len(ixs) == 0 or len(ds) == 0:
-                indices.append(0)
-                # dists.append(0.)
+                ix = self.take_random_zero_vector()
+                assert ix is not None
+                dist = 0.
             else:
-                if ixs[0] == y:
-                    indices.append(ixs[1])
-                    # dists.append(ds[1])
+                if ixs[0] == y_:
+                    ix, dist = ixs[1], -ds[1]
                 else:
-                    indices.append(ixs[0])
-                    # dists.append(ds[0])
+                    ix, dist = ixs[0], -ds[0]
+                if dist <= 0:
+                    zero_ix = self.take_random_zero_vector()
+                    if zero_ix is not None and zero_ix != y_: # TODO: if zero_ix == y, sample other ix if possible
+                        ix = zero_ix
+                        dist = 0.
+            indices.append(ix)
+            # dists.append(dist)
         return indices
 
-    def update(self, ixs, new_values):
-        del_strategy = 0
+    def update(self, ixs: np.ndarray, new_values: ss.csr_matrix):
+        # print("to del: ", ixs_del)
         ixs_set = set(ixs)
         ixs_del = list(self.present & ixs_set)
+        del_strategy = 0
         self.index.deleteBatch(ixs_del, del_strategy)
-        self.present |= ixs_set
-        self.index.addBatch(new_values, ixs)
+        self.not_present -= ixs_set
+        # print("to add: ", ixs, " cur: ", self.present)
+        ixs_nz, ixs_z = [], []
+        for ix, v in enumerate(new_values):
+            if v.nnz == 0:
+                ixs_z.append(ix)
+            else:
+                ixs_nz.append(ix)
+        self.index.addBatch(new_values[ixs_nz], ixs[ixs_nz])
+        self.present |= set(ixs[ixs_nz])
+        self.present -= set(ixs[ixs_z])
